84b98a801533973e38fb05dbfa5882aa46b5cd64
SwG Ghosh <codecrafts.cf@icloud.com>
2017-12-28 01:14:48 +0530

    Added introductory text in services page

--- colorchange.js
+++ colorchange.js
@@ -1,4 +1,4 @@
 var colorChange = function() {
     document.body.style.backgroundColor = '#2' + (Math.random() * 7).toFixed(0) + (Math.random() * 8).toFixed(0);
 }
-var i = setInterval(colorChange, 4000);
+var interval = setInterval(colorChange, 4000);
